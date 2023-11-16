57330189f5a90263dc3ce4e2b34585569cade6f5
KinoiraLink <64769156+KinoiraLink@users.noreply.github.com>
2023-11-16 15:49:09 +0800

    完成 第六节 第七节 第八节

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
+    private const int DIRECTION = 1;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+        //方向
+        if (Input.GetKey(KeyCode.W))
+            inputVector.y = +DIRECTION;
+        if (Input.GetKey(KeyCode.S))
+            inputVector.y = -DIRECTION;
+        if (Input.GetKey(KeyCode.A))
+            inputVector.x = -DIRECTION;
+        if (Input.GetKey(KeyCode.D))
+            inputVector.x = +DIRECTION;
+        //移动
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        //转向
+        transform.forward = Vector3.Slerp(transform.forward,moveDir,rotateSpeed * Time.deltaTime);
+    }
+}

aec09a81eb9e47db8d981af608852c5a2dcb2fae
KinoiraLink <64769156+KinoiraLink@users.noreply.github.com>
2024-01-29 16:30:17 +0800

    写到第19节，发现一个移动的bug不知道之后会不会解决

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,148 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour,IKitchenObjectParent
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+    [SerializeField] private Transform kitchenObjectHoldPoint;
+
+    private KitchenObject kitchenObject;
+
+    private BaseCounter selectedCounter;
+
+    public static Player Instance { get;private set; }
+
+    public event EventHandler<OnSelectedCounterChangeEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangeEventArgs : EventArgs {
+        public BaseCounter selectedCounter;
+    }
+
+    public void Awake()
+    {
+        if (Instance != null)
+            Debug.LogError("There is more than one Player instance");
+        Instance = this;
+
+    }
+
     private const int DIRECTION = 1;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
-    [SerializeField]private GameInput gameInput;
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if(selectedCounter != null)
+            selectedCounter.Interact(this);
+    }
+
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+
+
+    }
+
+    public void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
 
-        
-        isWalking = moveDir !=Vector3.zero;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else { }
+
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+        transform.forward = moveDir;
+
+        isWalking = moveDir != Vector3.zero;
 
         //转向
-        transform.forward = Vector3.Slerp(transform.forward,moveDir,rotateSpeed * Time.deltaTime);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+
+    }
+
+    public void HandleInteractions() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        if (moveDir != Vector3.zero)
+            lastInteractDir = moveDir;
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycasthit, interactDistance, countersLayerMask))
+            if (raycasthit.transform.TryGetComponent(out BaseCounter baseCounter))
+            {
+                SelectedCounter(baseCounter);
+            }
+            else
+                SelectedCounter(null);
+        else
+            SelectedCounter(null);
+
+
+
+    }
+
+    private void SelectedCounter(BaseCounter baseCounter)
+    {
+        this.selectedCounter = baseCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangeEventArgs
+        {
+            selectedCounter = selectedCounter
+        }) ;
     }
 
     public bool IsWalking() {
         return isWalking;
     }
+
+    public void ClearKitchenObject() => kitchenObject = null;
+
+    public bool HasKitchenObject() => kitchenObject != null;
+
+
+    public void SetKitchenObject(KitchenObject kitchenObject) => this.kitchenObject = kitchenObject;
+
+
+    public Transform GetKitchenObjectFollowTransform() => kitchenObjectHoldPoint;
+
+    public KitchenObject GetKitchenObject() => kitchenObject;
 }
